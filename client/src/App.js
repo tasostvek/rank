66ec6e1ff15431bed0b34eed8eeef40fad48054d
Anastasios Tziveks <tuf91473@temple.edu>
2020-09-21 18:32:33 -0400

    Added React Bootstrap and used Forms

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1>Rank</h1>
+        <div className="input-container">
+            <Form inline className="label-input">
+              <Form.Group>
+                <Form.Label className="email-label">Email Address</Form.Label>
+                <Form.Control className="input-field" type="email" placeholder="Enter email"/>
+              </Form.Group>
+            </Form>
+            <Form inline className="label-input">
+              <Form.Group>
+                <Form.Label className="password-label">Password</Form.Label>
+                <Form.Control className="input-field" type="password" placeholder="Enter password"/>
+              </Form.Group>
+            </Form>
+            <Button>Submit</Button>
+          </div>
       </header>
     </div>
   );
