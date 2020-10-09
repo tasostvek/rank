db17d86b804bd531f36a41a0073b36eeafc636ac
Anastasios Tziveks <tuf91473@temple.edu>
2020-10-09 14:18:32 -0400

    Adjusted responsiveness of layout

--- client/src/Routes/Nav.js
+++ client/src/Routes/Nav.js
@@ -9,6 +9,7 @@ const Nav = () =>  {
             <ul className= "menu-ul"id="menu">
                 <Link to="/"><li href="#" className="title" id="logo">RideRank</li></Link>
                 <div className = "menu-align">
+                    <Link to="/"><li href="#" className ="menu-option">Home</li></Link>
                     <Link to="/SignUp"><li href="#" className ="menu-option">Sign Up</li></Link>
                     <Link to="/Rank"><li href="#"className ="menu-option last">Rank</li></Link>
                 </div>
