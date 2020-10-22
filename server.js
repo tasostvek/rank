09e54f27264975fe9c5beb98d4b0870766110513
Anastasios Tziveks <tuf91473@temple.edu>
2020-10-22 19:49:34 -0400

    Made adjustment to server.js

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(express.urlencoded({ extended: false}));
 
 
 if(process.env.NODE.ENV === 'production'){
-    app.use(express.static(path.join(__dirname,'./client/build')));
+    app.use(express.static('./client/build')); 
     app.get('*', function (req, res){
         res.sendFile(path.join(__dirname, './client/build','index.html'));
     });
