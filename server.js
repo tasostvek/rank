a915f6107055e0fb165ff491b34ddb689e0f4f1f
Anastasios Tziveks <tuf91473@temple.edu>
2020-11-13 00:49:04 -0500

    Change to express.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({ limit: '50mb',extended: false}));
 
 //app.use(express.static("client/build"));
-app.use(express.static('client/build'));
+app.use(express.static(path.join(__dirname, 'client', 'build')));
 app.get('/*', (req,res) =>{
     res.sendFile(path.join(__dirname,'client','build','index.html'));
 });
