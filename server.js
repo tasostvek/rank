003b5b5515c9491b9147112d9f6a7bba5a58ca47
Anastasios Tziveks <tuf91473@temple.edu>
2020-11-12 23:42:48 -0500

    Slight change to server.js

--- server.js
+++ server.js
@@ -29,10 +29,18 @@ app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({ limit: '50mb',extended: false}));
 
 //app.use(express.static("client/build"));
-app.use(express.static('client/build'));
+/*app.use(express.static('client/build'));
 app.get('/', (req,res) =>{
     res.sendFile(path.join(__dirname,'client','build','index.html'));
-});
+});*/
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '', 'build')));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, 'front_end', 'build', 'index.html'))
+    });
+    
+}
 
 
 app.use(cors());
