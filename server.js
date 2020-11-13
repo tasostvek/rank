a017d5105a89bf4c070eaea55b33104dcc77ab61
Anastasios Tziveks <tuf91473@temple.edu>
2020-11-13 00:17:44 -0500

    Change to procfile

--- server.js
+++ server.js
@@ -29,17 +29,17 @@ app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({ limit: '50mb',extended: false}));
 
 //app.use(express.static("client/build"));
-/*app.use(express.static('client/build'));
+app.use(express.static('client/build'));
 app.get('/', (req,res) =>{
     res.sendFile(path.join(__dirname,'client','build','index.html'));
-});*/
-if(process.env.NODE.ENV === 'production') {
+});
+/*if(process.env.NODE.ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'client', 'build')));
   
     app.get('*', (req, res) => {
       res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
     }); 
-}
+}*/
 
 app.use(cors());
 app.use(morgan('tiny'));
